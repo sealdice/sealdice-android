1d5da70fe4301c709166b5e83dcd53ec3b966756
Pinenutn <2417971099@qq.com>
2024-02-11 11:46:51 +0800

    添加功能：保活策略：最近任务列表不显示-Fix1

--- app/src/main/java/com/sealdice/dice/MyApplication.kt
+++ app/src/main/java/com/sealdice/dice/MyApplication.kt
@@ -4,7 +4,7 @@ import android.app.Application
 import android.content.Context
 import androidx.preference.PreferenceManager
 import com.sealdice.dice.utils.Utils.setHideTaskStatus
-//import com.sealdice.dice.secrets.Auth.*
+import com.sealdice.dice.secrets.Auth.*
 import org.acra.config.httpSender
 import org.acra.data.StringFormat
 import org.acra.ktx.initAcra
@@ -29,15 +29,15 @@ class MyApplication : Application() {
     override fun attachBaseContext(base: Context) {
         super.attachBaseContext(base)
 
-//        initAcra {
-//            reportFormat = StringFormat.JSON
-//            httpSender {
-//                uri = ACRA_URL
-//                basicAuthLogin = ACRA_BASIC_AUTH
-//                basicAuthPassword = ACRA_LOGIN_PASS
-//                httpMethod = HttpSender.Method.POST
-//            }
-//        }
+        initAcra {
+            reportFormat = StringFormat.JSON
+            httpSender {
+                uri = ACRA_URL
+                basicAuthLogin = ACRA_BASIC_AUTH
+                basicAuthPassword = ACRA_LOGIN_PASS
+                httpMethod = HttpSender.Method.POST
+            }
+        }
 
     }
 }
